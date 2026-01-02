69fc19c61be5397b47bb40f53f10bbfc8379cb00
Satyam Anmol <bytebond1@gmail.com>
2026-01-02 08:58:24 +0530

    added type checker.py

---
+++ Python Core Foundations/type_checker.py
@@ -0,0 +1,9 @@
+num = input("Enter a number: ")
+
+int_value = int(num)
+float_value = float(num)
+str_value = str(num)
+
+print("Integer value:", int_value, "Type:", type(int_value))
+print("Float value:", float_value, "Type:", type(float_value))
+print("String value:", str_value, "Type:", type(str_value))
