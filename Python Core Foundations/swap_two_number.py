eb49bea0b59a503cd0b391dcf2b8df431231937b
Satyam Anmol <bytebond1@gmail.com>
2026-01-02 08:55:47 +0530

    added swap two number.py

---
+++ Python Core Foundations/swap_two_number.py
@@ -0,0 +1,7 @@
+#Swap Two Numbers withoud using third variable
+num1=float(input("enter the first number:"))
+num2=float(input("enter the second number"))
+num1,num2=num2,num1
+print("AFTER SWAPING")
+print("first number=",num1)
+print("second number=",num2)
