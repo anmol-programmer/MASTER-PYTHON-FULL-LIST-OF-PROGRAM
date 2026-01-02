7d710e03eb45a68c99f8a08221b5545e5c52a4c4
Satyam Anmol <bytebond1@gmail.com>
2026-01-02 23:41:21 +0530

    added mutability proof

---
+++ Deep Concept/Mutability_proof.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3]
+
+print("Before modification:")
+print("List:", numbers)
+print("ID:", id(numbers))
+
+numbers.append(4)
+
+print("\nAfter modification:")
+print("List:", numbers)
+print("ID:", id(numbers))
