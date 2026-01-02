06205e89b68585a95bbfe2f71d0d0851a8406a8f
Satyam Anmol <bytebond1@gmail.com>
2026-01-02 09:03:37 +0530

    added boolean logic check.py

---
+++ Python Core Foundations/boolean_logic_check.py
@@ -0,0 +1,7 @@
+a = bool(int(input("Enter first boolean (1 for True, 0 for False): ")))
+b = bool(int(input("Enter second boolean (1 for True, 0 for False): ")))
+
+print("AND result:", a and b)
+print("OR result:", a or b)
+print("NOT a result:", not a)
+print("NOT b result:", not b)
