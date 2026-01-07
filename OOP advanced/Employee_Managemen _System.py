77b624a3a0406661734addf4337e2ca133e0ee40
Satyam Anmol <bytebond1@gmail.com>
2026-01-07 22:49:59 +0530

    added employee management system

--- OOP advanced/Employee_Managemen _System.py
+++ OOP advanced/Employee_Managemen _System.py
@@ -1,10 +1,48 @@
 class Employee:
-    def __init__(self, name, salary):
+    def __init__(self, name, base_salary):
         self.name = name
-        self.salary = salary
+        self.base_salary = base_salary
 
-    def display_salary(self):
-        print("Employee Name:", self.name)
-        print("Salary:", self.salary)
-emp1 = Employee("Satyam", 50000)
-emp1.display_salary()
+    def calculate_salary(self):
+        return self.base_salary
+
+class Developer(Employee):
+    def __init__(self, name, base_salary, bonus):
+        super().__init__(name, base_salary)
+        self.bonus = bonus
+
+    def calculate_salary(self):
+        return self.base_salary + self.bonus
+
+class Manager(Employee):
+    def __init__(self, name, base_salary, bonus, allowance):
+        super().__init__(name, base_salary)
+        self.bonus = bonus
+        self.allowance = allowance
+
+    def calculate_salary(self):
+        return self.base_salary + self.bonus + self.allowance
+
+dev = Developer("satyam", 50000, 10000)
+mgr = Manager("anmol", 70000, 15000, 8000)
+
+print(f"Developer Salary: {dev.calculate_salary()}")
+print(f"Manager Salary: {mgr.calculate_salary()}")
+
+"""1️⃣ Inheritance – Employee Management System
+
+Concept: Inheritance, method overriding
+
+Problem:
+Create a base class Employee with:
+name
+base_salary
+method calculate_salary()
+
+Create child classes:
+
+Developer → salary = base + bonus
+
+Manager → salary = base + bonus + allowance
+
+📌 Job Use: HR payroll systems"""
